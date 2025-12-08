79b78231563839ced86e9395268a0b347e362238
Pratik <pratik@example.com>
2025-12-08 16:16:59 +0530

    ok1

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import NavBar from './NavBar';
+import NavBar from './Navbar';
 const Layout = () => {
     return (
         <div className="min-h-screen text-slate-800 font-sans selection:bg-indigo-500 selection:text-white">
